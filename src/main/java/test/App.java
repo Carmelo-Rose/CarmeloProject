1931b11660b88c9976a85a685611367480eb6530
554221991@qq.com <65816813yingkuai>
2019-03-25 15:55:04 +0800

    opo

---
+++ src/main/java/test/App.java
@@ -0,0 +1,26 @@
+package test;
+
+/**
+ * 内部類
+ */
+public class App {
+    public static void main(String[] args) {
+        Outer.Inner oi = new Outer.Inner();
+        oi.print();
+    }
+
+}
+class Outer {
+    static class Inner {
+        private String str = "内部類";
+
+        public void print() {
+            System.out.println(str+"方法");
+        }
+    }
+
+    public void fun() {
+        Inner inner = new Inner();
+        System.out.println(inner.str);
+    }
+}
