9992c3a5f0ff7dd9f2bac1f97114a00f63f2cf21
554221991@qq.com <65816813yingkuai>
2019-03-26 17:13:13 +0800

    ·

--- src/main/java/test/Sys.java
+++ src/main/java/test/Sys.java
@@ -3,6 +3,6 @@ package test;
 public class Sys {
     public static void main(String[] args){
         System.out.println("測試Git提交！");
-        System.out.println("測試Git6提交！");
+        System.out.println("測試Git2提交！");
     }
 }
