4e3ecd8aaa3ee8579574f51bb643843133ea39a1
何盈快 <554221991@qq.com>
2019-03-26 16:00:11 +0800

    更新 Sys.java 在碼云提交一條語句！

--- src/main/java/test/Sys.java
+++ src/main/java/test/Sys.java
@@ -4,5 +4,6 @@ public class Sys {
     public static void main(String[] args){
         System.out.println("測試Git提交！");
         System.out.println("上一條成功，測試多添加一條！");
+        System.out.println("在碼云添加了一條輸出語句！");
     }
 }
