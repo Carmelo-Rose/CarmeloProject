02e40fee5b1ab524fb47f7ce77d5fce334b57cbf
何盈快 <554221991@qq.com>
2019-03-28 11:02:42 +0800

    更新 Sys.java

--- src/main/java/test/Sys.java
+++ src/main/java/test/Sys.java
@@ -4,5 +4,6 @@ public class Sys {
     public static void main(String[] args){
         System.out.println("測試Git提交！");
         System.out.println("測試Git提交！");
+        System.out.println("測試Git提交！");
     }
 }
